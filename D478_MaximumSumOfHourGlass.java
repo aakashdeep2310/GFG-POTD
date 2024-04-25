10ed84d3b9a6d524dd529b8726c5236e448affa3
aakashdeep2310 <rjaakashdeep007@gmail.com>
2024-04-26 01:19:41 +0530

    first commit

---
+++ D478_MaximumSumOfHourGlass.java
@@ -0,0 +1,24 @@
+public class D478_MaximumSumOfHourGlass {
+
+    public static void main(String[] args) {
+        
+    }
+
+    public static int findMaxSum(int x, int y, int m[][]){
+        if(x<=2 || y<= 2){
+            return -1;
+        }
+
+        int ans = Integer.MIN_VALUE;
+        for(int i = 0; i<x-2; i++){
+            for(int j = 0; j<y-2; j++){
+                int sum = m[i][j] + m[i][j+1] + m[i][j+2] + 
+                m[i+1][j+1] + m[i+2][j] + m[i+2][j+1] + m[i+2][j+2];
+
+                ans = Math.max(ans, sum);
+            }
+        }
+        return ans;
+    }
+    
+}
