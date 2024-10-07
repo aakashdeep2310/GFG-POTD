511e1179bc979c3ebd9957f3bed275b49c307702
Aakash Deep <90546941+aakashdeep2310@users.noreply.github.com>
2024-10-07 22:48:48 +0530

    Max Sum Subarray of size K GFG

---
+++ MaxSumSubarrayofsizeK.java
@@ -0,0 +1,27 @@
+
+//User function Template for Java
+class Solution{
+    static long maximumSumSubarray(int K, ArrayList<Integer> Arr,int N){
+        // code here
+        
+        long maxSum = 0;
+        long sum = 0;
+        //first find the sum of K elements
+        int index = 0;
+        while(index<N && index < K){
+            sum += Arr.get(index);
+            index++;
+        }
+        
+        //update the maxSum
+        maxSum = sum;
+        for(int i = 1; i<N-K+1; i++){
+            int prevElement = Arr.get(i-1);
+            int nextElement = Arr.get(i+K-1);
+            sum = sum - prevElement + nextElement;
+            maxSum = Math.max(maxSum, sum);
+        }
+        
+        return maxSum;
+    }
+}
