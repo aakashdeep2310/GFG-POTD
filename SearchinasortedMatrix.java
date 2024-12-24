e39f65217f09fd1a394e5da061345af8fc7ec4e5
Aakash Deep <90546941+aakashdeep2310@users.noreply.github.com>
2024-12-24 08:50:46 +0530

    Search in a sorted Matrix

---
+++ SearchinasortedMatrix.java
@@ -0,0 +1,32 @@
+class Solution {
+    // Function to search a given number in row-column sorted matrix.
+    public boolean searchMatrix(int[][] mat, int x) {
+        // code here
+        
+        int n = mat.length;
+        int m = mat[0].length;
+        
+        int start = 0;
+        int end = n * m -1;
+        
+       while(start<=end){
+           
+            int mid = start + (end - start) / 2;
+            
+            int row = mid / m;
+            int col = mid % m;
+            
+            if(mat[row][col] == x){
+                return true;
+            }else if(x < mat[row][col]){
+                end = mid - 1;
+            }else{
+                start = mid + 1;
+            }
+        }
+        
+        return false;
+        
+
+    }
+}
