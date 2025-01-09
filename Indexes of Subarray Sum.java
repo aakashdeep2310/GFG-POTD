df7326ccea9fbc5336890b6c7c03e2c8d518c041
Aakash Deep <90546941+aakashdeep2310@users.noreply.github.com>
2025-01-09 22:35:02 +0530

    Create Indexes of Subarray Sum.java

---
+++ Indexes of Subarray Sum.java
@@ -0,0 +1,22 @@
+class Solution {
+    static ArrayList<Integer> subarraySum(int[] arr, int target) {
+        // code here
+        ArrayList<Integer> ans = new ArrayList<>();
+        int n = arr.length, j = 0, sum = 0;
+        
+        for(int i = 0; i<n; i++){
+            sum += arr[i];
+            
+            while(sum>target && j<i){
+                sum -= arr[j++];
+            }
+            if(sum==target){
+                ans.add(j+1);
+                ans.add(i+1);
+                return ans;
+            }
+        }
+        ans.add(-1);
+        return ans;
+    }
+}
